eb67cae8b28f1c7aabd8a5bf6e03d8b45ca68ea5
duchieu <tranthaiduchieu@gmail.com>
2025-05-12 16:46:30 +0200

    done DWA in test

--- source/pplan/controllers/test/test.py
+++ source/pplan/controllers/test/test.py
@@ -14,7 +14,9 @@ MAX_VELOCITY = 26
 WHEEL_RADIUS = 0.043  # meters
 AXLE_LENGTH = 0.18    # meters (distance between left and right wheels)
 
-OBSTACLE_VALUE = 255
+OBSTACLE_VALUE = 1
+PATH_VALUE = 255
+PIXEL_DETECT_RADIUS = 40
 
 MAP_SIZE = 1000 # 1000cm = 10m
 RESOLUTION = 0.01 # 1cm for 1 grid cell
@@ -60,12 +62,12 @@ class MyRobot:
         """Get the angle between robot heading and X-axis in world coordinates."""
         orientation = self.robot.getSelf().getOrientation()
         dir_x = orientation[0]
-        dir_y = orientation[1]
+        dir_y = orientation[3]
         angle_rad = np.arctan2(dir_y, dir_x)
         if type == 'rad':
             return angle_rad
         elif type == 'deg':
-            angle_deg = (np.degrees(angle_rad) + 360) % 360
+            angle_deg = np.degrees(angle_rad)
             return angle_deg
 
     def get_angle_diff(self, map_target, type='deg'):
@@ -118,14 +120,14 @@ class MyRobot:
 
     def get_local_target(self):
         map_x, map_y = self.get_map_position()
-        r = 10
+        r = PIXEL_DETECT_RADIUS
 
         intersect_list = []
         for x_border in [-r, r]:
             for j in range(-r, r+1):
                 point_x = map_x + x_border
                 point_y = map_y + j
-                if self.grid_map[point_y, point_x] == 100 and self.get_angle_diff([point_x, point_y]) <= 90:
+                if self.grid_map[point_y, point_x] == PATH_VALUE and self.get_angle_diff([point_x, point_y]) <= 90:
                     visual_map[point_y, point_x] = (0,0,255)
                     intersect_list.append(np.array([point_x, point_y]))
 
@@ -133,15 +135,15 @@ class MyRobot:
             for i in range(-r, r+1):
                 point_x = map_x + i
                 point_y = map_y + y_border
-                if self.grid_map[point_y, point_x] == 100 and self.get_angle_diff([point_x, point_y]) <= 90:
+                if self.grid_map[point_y, point_x] == PATH_VALUE and self.get_angle_diff([point_x, point_y]) <= 90:
                     visual_map[point_y, point_x] = (0,0,255)
                     intersect_list.append(np.array([point_x, point_y]))
 
         if len(intersect_list) == 0:
-            print("No intersection found")
             return None
         else:
             dist = [np.linalg.norm(intersect_point - np.array([map_x, map_y])) for intersect_point in intersect_list]
+            print('found path-------')
             return intersect_list[np.argmin(dist)]
 
     def there_is_obstacle(self, map_target):
@@ -151,14 +153,13 @@ class MyRobot:
 
     def dwa_planner(self, world_target):
         MAX_SPEED = MAX_VELOCITY * self.wheel_radius
-        
+        # r = v / w
         v_samples = [0.08, 0.1, 0.15, 0.2, 0.5]
         # w_samples = [0.2, 0.5, 1, 1.5]
-        w_samples = [0.2, -0.2, 0.5, -0.5, 0, 1, -1, 1.5, -1.5]
-        # w_samples += [-w for w in w_samples]
+        w_samples = [0, 2, -2, 4, -4, 4.5, -4.5]
 
         best_score = -float('inf')
-        min_error = float('inf')
+        # min_error = float('inf')
         best_v = 0.0
         best_w = 0.0
 
@@ -196,29 +197,29 @@ class MyRobot:
                 predicted_angle_to_target = np.arctan2(world_target[1]-cy, world_target[0]-cx)
                 heading_error = get_angle_diff(predicted_angle_to_target, ct)
 
-                error = 4*np.sin(heading_error) + 2*predicted_distance + 0.5*(MAX_SPEED - v)
-                if error < min_error:
+                heading_score = np.cos(heading_error)
+                distance_score = 1 - (predicted_distance / 2)  # you can fix some max, e.g., 2m
+                speed_score = v / MAX_SPEED
+                score = 4.0 * heading_score + 2.0 * distance_score + speed_score
 
-                    print(f"v: {v}, w: {w}")
-                    print(f"Predicted angle to target: {np.degrees(predicted_angle_to_target)}")
-                    print(f"Heading error: {np.degrees(heading_error)}")
-                    print(f"Distance: {current_distance} {predicted_distance}")
-                    print(f"New min error: {error}")
-                    min_error = error
+                if score > best_score:
+
+                    # print(f"Predicted angle to target: {np.degrees(predicted_angle_to_target)}")
+                    # print(f"Heading error: {np.degrees(heading_error)}")
+                    # print(f"Distance: {current_distance} {predicted_distance}")
+                    # print(f"New min error: {error}")
+                    best_score = score
                     best_v = v
                     best_w = w
 
-
-
+        # print('angle diff: ', heading_error)
+        # print(f"v: {best_v}, w: {best_v}, best score: {best_score}") 
         return best_v, best_w
 
-    def dwa_plan(self, map_target, tolerance=0.2):
-        # distance = np.linalg.norm(map_target - current_pos)
-
-        # if distance < tolerance:
-        #     print("[dwa_plan] Reached target!")
-        #     self.stop_motor()
-        #     return
+    def dwa_plan(self, map_target):
+        if self.get_map_distance(map_target) < 2:
+            self.stop_motor()
+            return True
 
         world_target = self.convert_to_world_coordinates(map_target[0], map_target[1])
         v, w = self.dwa_planner(world_target)
@@ -227,6 +228,12 @@ class MyRobot:
         print("Left speed: ", left_speed, "Right speed: ", right_speed)
 
         self.set_robot_velocity(left_speed, right_speed)
+        return False
+
+    def get_map_distance(self, target):
+        a = self.get_map_position()
+        return np.linalg.norm(a - np.array(target))
+
 
 def get_angle_diff(a, b):
         diff = a - b
@@ -235,6 +242,7 @@ def get_angle_diff(a, b):
         while diff < -np.pi:
             diff += 2*np.pi
         return diff
+
 
 robot = MyRobot()
 
@@ -304,37 +312,116 @@ def draw_robot_orientation(screen, robot, window_size):
 
     draw_infinite_line(screen, (0, 255, 0), p1, p2, width=2)
 
+def generate_random_curved_path(start_point, end_point):
+    step_size=50
+    deviation=5
+    
+    x0, y0 = start_point
+    x1, y1 = end_point
+
+    dx = x1 - x0
+    dy = y1 - y0
+    distance = np.hypot(dx, dy)
+
+    if distance == 0:
+        return [start_point]
+
+    num_steps = max(1, int(distance // step_size))
+
+    points = []
+    for i in range(num_steps + 1):
+        t = i / num_steps
+        x = x0 + t * dx
+        y = y0 + t * dy
+
+        # Thêm random nhỏ vào mỗi bước để tạo cong
+        offset_x = random.randint(-deviation, deviation)
+        offset_y = random.randint(-deviation, deviation)
+
+        x += offset_x
+        y += offset_y
+
+        points.append((int(round(x)), int(round(y))))
+
+    # Lọc duplicate points
+    filtered_points = []
+    seen = set()
+    for p in points:
+        if p not in seen:
+            filtered_points.append(p)
+            seen.add(p)
+
+    # Đảm bảo điểm cuối đúng end_point
+    if filtered_points[-1] != (int(round(x1)), int(round(y1))):
+        filtered_points[-1] = (int(round(x1)), int(round(y1)))
+
+    return filtered_points
+
+def generate_path():
+    start_point = [200, 250]
+    path = []
+    end_x = 400
+    end_y = 400
+    for i in range(3):
+        random_int = random.randint(200, 350)
+        if i % 2 == 0:
+            end_x = start_point[0] + random_int
+        else:
+            end_y = start_point[1] + random_int
+
+        path +=  generate_random_curved_path(start_point, (end_x, end_y))
+        start_point = [end_x + 30, end_y+ 30]
+
+    return path
+
+def draw_path(path):
+    # Scale path theo kích thước màn hình
+    scale_x = window_size[0] / MAP_SIZE
+    scale_y = window_size[1] / MAP_SIZE
+    scaled_path = [(int(x * scale_x), int(y * scale_y)) for (x, y) in path]
+
+    # Vẽ từng điểm
+    for point in scaled_path:
+        pygame.draw.circle(screen, (255, 0, 0), point, 1)  # chấm đỏ, bán kính 2 pixel
+
+window_size = (500, 500)
+screen = init_pygame(window_size)
+
 def main():
     try:
         window_size = (500, 500)
-        # screen = init_pygame(window_size)
-        last_target = robot.get_map_position()
+        screen = init_pygame(window_size)
 
         running = True
+        path = generate_path()
+        target_index = 0
+        target = path[target_index]
         while robot.step(TIME_STEP) != -1 and running:
-            # for event in pygame.event.get():
-            #     if event.type == pygame.QUIT:
-            #         running = False
-
-            # _target = robot.get_local_target()
-            # target = _target if _target is not None else last_target
-            # last_target = target
-            # robot.draw_position_in_map(visual_map)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            print(len(path),'----------------')
+            screen.fill((0, 0, 0))
+            robot.draw_position_in_map(visual_map)
 
             # map_surface = create_map_surface(visual_map, window_size)
-
             # screen.blit(map_surface, (0, 0))
 
-            # draw_robot_to_target(screen, robot, target, window_size)
-            # draw_robot_orientation(screen, robot, window_size)
+            draw_path(path)
+            draw_robot_to_target(screen, robot, target, window_size)
+            draw_robot_orientation(screen, robot, window_size)
 
-            # pygame.display.flip()
+            pygame.display.flip()
 
-            # print("Target: ", target)
-            # robot.dwa_plan(target)
-            # print('---------------------------')
-            print("heading: ", robot.get_heading('deg'))
-            time.sleep(1)
+            if robot.dwa_plan(target):
+                target_index += 1
+                if target_index < len(path):
+                    target = path[target_index]
+                else:
+                    print("Reached the end of the path.")
+                    break
+            print('---------------------------')
+            # time.sleep(0.1)
 
         pygame.quit()
 
